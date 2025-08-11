d1c06e5bdb0e8216d4a744f727c2dc555ee905bb
Salman Islam Polok <salmanpolok224@gmail.com>
2025-08-11 23:44:11 +0600

    added js compare

---
+++ Javascript/Module-18/1.js
@@ -0,0 +1,34 @@
+// Comparition 
+```js
+//  5 ki 10 ar boro?   nah "False"
+console.log( 5 > 10);
+
+//  5 ki 10 ar soto?   yes "true"
+console.log( 5 < 10);
+
+//  5 ki 10 ar soman?   no "false"
+console.log( 5 == 10);
+
+//  5 ki 10 ar soman? athoba boro?   no "false"
+console.log( 5 >= 10);
+
+//  5 ki 10 ar soman? athoba soto?  yes boro "true"
+console.log( 5 <= 10);
+
+//  5 are 6 ki ashaman? yes "True"
+console.log( 5 !== 6);
+
+//  3ta somaon symble diye type cheke kora hoy 
+ //10 are string 11 soman? no  "False"
+console.log( 10 === '10');
+```
+
+// Variable diye coprasition cheke kora 
+```js
+//apple ar dam ki orange ar damer cheye besi ? ba soman? yes "true"
+const applePrice = 250;
+const orangePrice = 150;
+
+const compare = applePrice >= orangePrice;
+console.log(compare);
+```
